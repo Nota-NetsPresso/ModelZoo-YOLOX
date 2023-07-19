88662043401a036c50dab1a27aa3e32ec0b74d61
kolonist-minjun <130522722+kolonist-minjun@users.noreply.github.com>
2023-07-19 14:55:17 +0900

    Update __init__.py
    Add YOLOX_netspresso, TEMPbind

--- yolox/models/__init__.py
+++ yolox/models/__init__.py
@@ -6,6 +6,8 @@ from .build import *
 from .darknet import CSPDarknet, Darknet
 from .losses import IOUloss
 from .yolo_fpn import YOLOFPN
-from .yolo_head import YOLOXHead
+from .yolo_head import YOLOXHead, YOLOXHead_1, YOLOXHead_2
 from .yolo_pafpn import YOLOPAFPN
 from .yolox import YOLOX
+from .yolox_netspresso import YOLOX_netspresso
+from .temp_bind import TEMPbind
