d2b998df851499acd5810ee04c459d5da1c8745c
kolonist-minjun <130522722+kolonist-minjun@users.noreply.github.com>
2023-07-19 14:49:06 +0900

    Update yolox_nano.py
    Modify exp for yolox_nano to be usable for NetsPresso

--- exps/default/yolox_nano.py
+++ exps/default/yolox_nano.py
@@ -22,7 +22,7 @@ class Exp(MyExp):
         self.enable_mixup = False
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
-    def get_model(self, sublinear=False):
+    def get_model(self, sublinear=False, netspresso=False):
 
         def init_yolo(M):
             for m in M.modules():
@@ -30,19 +30,33 @@ class Exp(MyExp):
                     m.eps = 1e-3
                     m.momentum = 0.03
         if "model" not in self.__dict__:
-            from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
+            from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead, YOLOXHead_1
             in_channels = [256, 512, 1024]
             # NANO model use depthwise = True, which is main difference.
             backbone = YOLOPAFPN(
                 self.depth, self.width, in_channels=in_channels,
                 act=self.act, depthwise=True,
             )
-            head = YOLOXHead(
-                self.num_classes, self.width, in_channels=in_channels,
-                act=self.act, depthwise=True
-            )
+            if netspresso:
+                head = YOLOXHead_1(
+                    self.num_classes, self.width, in_channels=in_channels,
+                    act=self.act, depthwise=True
+                )
+            else:
+                head = YOLOXHead(
+                    self.num_classes, self.width, in_channels=in_channels,
+                    act=self.act, depthwise=True
+                )
             self.model = YOLOX(backbone, head)
 
         self.model.apply(init_yolo)
         self.model.head.initialize_biases(1e-2)
         return self.model
+    
+    def get_head(self):
+        from yolox.models import YOLOXHead_2
+        
+        if "head" not in self.__dict__:
+            in_channels = [256, 512, 1024]
+            self.head = YOLOXHead_2(self.num_classes, in_channels=in_channels)
+        return self.head
