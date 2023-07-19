15f704c70a0e0e8569d2ee00410e09846375b4ac
kolonist-minjun <130522722+kolonist-minjun@users.noreply.github.com>
2023-07-19 14:48:30 +0900

    Update yolov3.py
    Modify exp for yolov3 to be usable for NetsPresso

--- exps/default/yolov3.py
+++ exps/default/yolov3.py
@@ -16,18 +16,25 @@ class Exp(MyExp):
         self.width = 1.0
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
-    def get_model(self, sublinear=False):
+    def get_model(self, sublinear=False, netspresso=False):
         def init_yolo(M):
             for m in M.modules():
                 if isinstance(m, nn.BatchNorm2d):
                     m.eps = 1e-3
                     m.momentum = 0.03
         if "model" not in self.__dict__:
-            from yolox.models import YOLOX, YOLOFPN, YOLOXHead
+            from yolox.models import YOLOX, YOLOFPN, YOLOXHead, YOLOXHead_1
             backbone = YOLOFPN()
-            head = YOLOXHead(self.num_classes, self.width, in_channels=[128, 256, 512], act="lrelu")
+            head = YOLOXHead_1(self.num_classes, self.width, in_channels=[128, 256, 512], act="lrelu") if netspresso else YOLOXHead(self.num_classes, self.width, in_channels=[128, 256, 512], act="lrelu")
             self.model = YOLOX(backbone, head)
         self.model.apply(init_yolo)
         self.model.head.initialize_biases(1e-2)
 
         return self.model
+    
+    def get_head(self):
+        from yolox.models import YOLOXHead_2
+        
+        if "head" not in self.__dict__:
+            self.head = YOLOXHead_2(self.num_classes, in_channels=[128, 256, 512])
+        return self.head
