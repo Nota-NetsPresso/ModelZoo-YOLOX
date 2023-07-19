a4ce17110ca8eec1a774bff6ff3f2e1c794a7ee0
kolonist-minjun <130522722+kolonist-minjun@users.noreply.github.com>
2023-07-19 14:50:14 +0900

    Create yolox_s_netspresso.py
    Add yolox_s_netpsresso.py for NetsPresso

---
+++ exps/default/yolox_s_netspresso.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+
+import torch
+from yolox.exp import Exp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+        self.compressed_model = "/workspace/YOLOX/obj_com.pt"
+        self.head = "/workspace/YOLOX/model_head.pt"
+        
+    def get_model(self, sublinear=False, netspresso=False):
+        if "model" not in self.__dict__:
+            from yolox.models import YOLOX_netspresso
+            backbone = torch.load(self.compressed_model)
+            head = torch.load(self.head)
+            self.model = YOLOX_netspresso(backbone, head)
+            
+        if netspresso:
+            return backbone
+
+        return self.model
