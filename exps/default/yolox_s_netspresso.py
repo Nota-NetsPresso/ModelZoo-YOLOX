b64bd7dba11de3c026ca82161845ed7fed482449
kolonist-minjun <130522722+kolonist-minjun@users.noreply.github.com>
2023-07-20 10:12:16 +0900

    Update yolox_s_netspresso.py
    fix typo

--- exps/default/yolox_s_netspresso.py
+++ exps/default/yolox_s_netspresso.py
@@ -11,16 +11,19 @@ from yolox.exp import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
+        self.depth = 0.33
+        self.width = 0.50
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
-        self.compressed_model = "/workspace/YOLOX_nota/obj_com.pt"
-        self.head = "/workspace/YOLOX_nota/model_head.pt"
+
+        self.compressed_model = "/path/your/compressed_model.pt"
+        self.head = "/path/your/model_head.pt"
         
     def get_model(self, sublinear=False, netspresso=False):
         if "model" not in self.__dict__:
             from yolox.models import YOLOX_netspresso
             backbone = torch.load(self.compressed_model)
             head = torch.load(self.head)
-            self.model = YOLOX_netspresso(backbone, head)
+            self.model = YOLOX_netspresso(backbone=backbone, head=head)
             
         if netspresso:
             return backbone
